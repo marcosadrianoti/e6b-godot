2571308c498d5a88c7cec4ee25aa3b95d7877e3b
Marcos Adriano <marcosadriano.ti@gmail.com>
2024-07-12 11:44:54 -0300

    chore(assets): organize e6b's svg files

--- main.gd
+++ main.gd
@@ -2,4 +2,6 @@ extends Node2D
 
 func _ready():
 	$Camera2D.position = $StartPosition.position
-	$objeto.position = $StartPosition.position
+	$tas_arc.position = $StartPosition.position
+	$true_index.position = $StartPosition.position
+	$compass.position = $StartPosition.position
