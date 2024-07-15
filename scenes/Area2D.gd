0cb85b65d34506334825ac2464351e069e143a10
Marcos Adriano <marcosadriano.ti@gmail.com>
2024-07-14 21:18:35 -0300

    feat(Disk): vertical drag all elements when click and drag trueIndex

--- scenes/Area2D.gd
+++ scenes/Area2D.gd
@@ -1,11 +1,36 @@
 extends Area2D
 
+var dragging = false
+var drag_start_position = Vector2()
+var start_position_trueIndex = Vector2()
+var start_position_compass = Vector2()
+
+@onready var compass = get_node("../compass")
+
 func _ready():
 	pass
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Clique detectado na trueIndex!")
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			print("Clique detectado na trueIndex!")
+			dragging = true
+			print('drag', dragging)
+			drag_start_position = get_global_mouse_position()
+			start_position_trueIndex = global_position
+			start_position_compass = compass.global_position
+		else:
+			dragging = false
+			print('drag', dragging)
+			
+	elif event is InputEventMouseMotion and dragging:
+		var mouse_delta = event.position - drag_start_position
+		global_position = start_position_trueIndex + Vector2(0, mouse_delta.y)
+		compass.global_position = start_position_compass + Vector2(0, mouse_delta.y)
 
 func _process(delta):
 	pass
+
+
+func _on_mouse_exited():
+	dragging = false
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,3 +2,4 @@ extends Node2D
 
 func _ready():
 	$Camera2D.position = $StartPosition.position
+	$Disk.position = $StartPosition.position
